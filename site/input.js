01b37f916eea141abef5870a45dcecb452420abe
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2023-08-28 14:23:03 -0600

    Update input.js

--- site/input.js
+++ site/input.js
@@ -1,51 +1,67 @@
-// class mouse{
-//     static pos = { x: 0, y: 0 };
-//     static down = false;
-//     static right = false;
-//     static start(element=document.documentElement) {
-//         function mousemove(e) {
-//             let br = element.getBoundingClientRect();
-//             mouse.pos.x = e.clientX - br.left;
-//             mouse.pos.y = e.clientY - br.top;
-//         }
-
-//         function mouseup(e) {
-//             if(e.which == 1){
-//                 mouse.down = false;
-//             } else if(e.which == 3){
-//                 mouse.right = false;
-//             }
-//         }
-
-//         function mousedown(e) {
-//             mousemove(e);
-//             if(e.which == 1){
-//                 mouse.down = true;
-//             } else if(e.which == 3){
-//                 mouse.right = true;
-//             }
-//         }
-//         document.addEventListener('mousemove', mousemove);
-//         document.addEventListener('mouseup', mouseup);
-//         document.addEventListener('mousedown', mousedown);
-//         document.addEventListener('contextmenu',e=>{e.preventDefault()});
-//     }
-// }
+var FULLSCREEN = false;
+document.onfullscreenchange = () => {FULLSCREEN = !FULLSCREEN};
+/*class mouse{
+    static pos = { x: 0, y: 0 };
+    static down = false;
+    static right = false;
+    static transformPos(e){
+        var x,y;
+        var element = e.target;
+        let br = element.getBoundingClientRect();
+        if(FULLSCREEN){
+            let ratio = window.innerHeight/canvas.height;
+            let offset = (window.innerWidth-(canvas.width*ratio))/2;
+            x = map(e.clientX-br.left-offset,0,canvas.width*ratio,0,element.width);
+            y = map(e.clientY-br.top,0,canvas.height*ratio,0,element.height);
+        } else {
+            x = e.clientX - br.left;
+            y = e.clientY - br.top;
+        }
+        return {x,y};
+    }
+    static start(element=document.documentElement) {
+        function mousemove(e) {
+            let pos = mouse.transformPos(e);
+            mouse.pos.x = pos.x;
+            mouse.pos.y = pos.y;
+        }
+        function mouseup(e) {
+            if(e.which == 1){
+                mouse.down = false;
+            } else if(e.which == 3){
+                mouse.right = false;
+            }
+        }
+        function mousedown(e) {
+            if(e.target != element) return;
+            mousemove(e);
+            if(e.which == 1){
+                mouse.down = true;
+            } else if(e.which == 3){
+                mouse.right = true;
+            }
+        }
+        document.addEventListener('mousemove', mousemove);
+        document.addEventListener('mouseup', mouseup);
+        document.addEventListener('mousedown', mousedown);
+        document.addEventListener('contextmenu',e=>{e.preventDefault()});
+    }
+}*/
 class keys{
     static keys = [];
     static start(){
         function keydown(e){
-            keys.keys[e.key] = true;
+            keys.keys[e.key.toLowerCase()] = true;
         }
         function keyup(e){
-            keys.keys[e.key] = false;
+            keys.keys[e.key.toLowerCase()] = false;
         }
         document.addEventListener('keydown',keydown);
         document.addEventListener('keyup',keyup);
     }
     static down(key){
-        if(key in keys.keys){
-            return keys.keys[key];
+        if(key.toLowerCase() in keys.keys){
+            return keys.keys[key.toLowerCase()];
         }
         return false;
     }
@@ -54,17 +70,22 @@ class Touch{
     static touches = [];
     static resolved = [];
     static init(callback){
+
+        function fixid(id){
+            return Math.abs(id%10);
+        }
+
         document.on('touchstart',e=>{
             for(let touch of e.changedTouches){
-                Touch.touches[touch.identifier] = touch;
-                e.preventDefault();
+                Touch.touches[fixid(touch.identifier)] = touch;
+                // try{e.preventDefault()}catch(e){};
             }
         });
 
         document.on('touchmove',e=>{
             if(Touch.touches.filter(e=>e).length == 1){
                 for(let touch of e.changedTouches){
-                    let last_pos = Touch.touches[touch.identifier];
+                    let last_pos = Touch.touches[fixid(touch.identifier)];
                     callback({
                         type: 'scroll',
                         x: touch.clientX,
@@ -89,7 +110,7 @@ class Touch{
                             dy: touch.clientY - last_pos.clientY
                         });
                         touch.action='zoom';
-                        Touch.touches[touch.identifier] = touch;
+                        Touch.touches[fixid(touch.identifier)] = touch;
                     } else {
                         tmps.push({
                             x: last_pos.clientX,
@@ -122,11 +143,11 @@ class Touch{
 
         document.on('touchend',e=>{
             for(let touch of e.changedTouches){
-                let ot = Touch.touches[touch.identifier];
+                let ot = Touch.touches[fixid(touch.identifier)];
                 if(!ot.action){
                     let dx=ot.clientX-touch.clientX,dy=ot.clientY-touch.clientY;
+                    let br = ot.target.getBoundingClientRect();
                     if(Math.sqrt(dx**2+dy**2) < 5){
-                        let br = ot.target.getBoundingClientRect();
                         callback({
                             type:'click',
                             x:ot.clientX - br.x,
@@ -134,6 +155,14 @@ class Touch{
                             target:ot.target
                         });
                     }
+                } else {
+                    let br = ot.target.getBoundingClientRect();
+                    callback({
+                        type: 'end',
+                        x:ot.clientX - br.x,
+                        y:ot.clientY - br.y,
+                        target:ot.target
+                    });
                 }
                 Touch.touches[touch.identifier] = null;
             }
